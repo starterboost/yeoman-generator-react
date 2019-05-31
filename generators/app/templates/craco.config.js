d29f133110aaf9fcfb5d40379a77116fcb51fb47
jimib <jimi.bailey@gmail.com>
2019-05-31 16:08:05 +0100

    Added babel-loader to process any @starterboost modules by default

--- generators/app/templates/craco.config.js
+++ generators/app/templates/craco.config.js
@@ -1,11 +1,20 @@
 /* craco.config.js */
 const reactHotReloadPlugin = require('craco-plugin-react-hot-reload');
+const rewireBabelLoader = require("craco-babel-loader");
 const stylePlugin = require('@starterboost/react-utilities/craco').plugins.style;
 
 module.exports = function({ env }) {
 	return {
-		plugins: [{
+		plugins: [
+		{
 			plugin: reactHotReloadPlugin
+		},
+		{ 
+			plugin: rewireBabelLoader, 
+			options: { 
+				includes: [/node_modules\/@starterboost/],
+				excludes: [/node_modules\/(!@starterboost)/] //put things you want to include in array here
+      }
 		},
 		{
       plugin: stylePlugin({extname:'styl',loader:'stylus-loader'})
